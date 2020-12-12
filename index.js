3ec9415c334625e173eb8fb6f8e51a0b146e7c57
Paulo Santana <paulo.santana.r@gmail.com>
2020-12-12 14:13:46 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const axios = require("axios");
+const xml = require("xml");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const params = req.query;
+  console.log(params);
+  const response = await axios.get(
+    "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx",
+    {
+      params,
+    }
+  );
+
+  res.send(xml(response.data));
+});
+
+app.listen(PORT || 3000, () => {
+  console.log("escutando");
+});
